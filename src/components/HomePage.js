326f65d2fa4f19c9b038de4231639ae95d16fccf
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-31 18:52:08 +0530

    Apply styling to banner

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React from 'react';
 import Banner from '../asset/Banner.jpeg';
 
-const HomePage =()=>{
-    return (
-        <main>
-            <div className="banner-div">
-            <img src={Banner} alt="COVID19" className="img-banner"/>
-            <h2 className="banner-h2">COVID19 statestics<br></br>Over the world</h2>
-            </div>
-        </main>
-    );
-};
+const HomePage = () => (
+  <main>
+    <div className="banner-div">
+      <img src={Banner} alt="COVID19" className="img-banner" />
+      <h2 className="banner-h2">
+        COVID19 statestics
+        <br />
+        Over the world
+      </h2>
+    </div>
+  </main>
+);
 
 export default HomePage;
