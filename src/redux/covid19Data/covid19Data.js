52d0bf8a8b5920f0d40de5398644880eda1090c9
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-31 20:25:29 +0530

    Add actions

--- src/redux/covid19Data/covid19Data.js
+++ src/redux/covid19Data/covid19Data.js
@@ -1,6 +1,18 @@
+import Axios from "axios";
+
 
 // constants
 const FETCH_DATA = 'covidMetrics/covid19Data/FETCH_DATA';
+
+// actions
+
+const fetchData = (payload) => ({
+  type: FETCH_DATA,
+  payload,
+});
+
+
+// APIs-functions
 
 
 // initial-state
