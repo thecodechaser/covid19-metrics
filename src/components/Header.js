e4f61ab91f654a1e0f9e19c6afad287aa9dbaa9f
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-31 17:58:44 +0530

    Add mic and settings icons on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { BsFillMicFill } from "@react-icons/all-files/bs/BsFillMicFill";
+import { IoIosSettings } from "@react-icons/all-files/io/IoIosSettings";
 
 const Header = (props)=>{
     const heading = props.heading;
@@ -6,6 +8,8 @@ const Header = (props)=>{
         <header>
             <div className="header-top">
                 <h3 className="header-h3">{heading}</h3>
+                <BsFillMicFill />
+                <IoIosSettings />
             </div>
         </header>
     );
