f0c368adf706258c841f50ba23bdf6dd74ef0dd9
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-31 20:22:49 +0530

    Add constants

--- src/redux/covid19Data/covid19Data.js
+++ src/redux/covid19Data/covid19Data.js
@@ -1,3 +1,12 @@
+
+// constants
+const FETCH_DATA = 'covidMetrics/covid19Data/FETCH_DATA';
+
+
+// initial-state
+const initialState = [];
+
+// reducer
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     default:
