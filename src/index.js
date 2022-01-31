b90b6f3bf2d47168cc98630242a958ab0263573e
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-31 20:47:35 +0530

    Wrappe App in provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
+    <Router>
     <App />
+    </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
