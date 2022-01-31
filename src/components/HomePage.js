ba469d4435e4b79472c962282c394ab21c8d8094
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-31 20:09:30 +0530

    Add search bar

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -11,6 +11,11 @@ const HomePage = () => (
         Over the world
       </h2>
     </div>
+    <div className="search-bar">
+        <h3 className="search-bar-h3">Search by country name</h3>
+        <input type="text" placeholder="Search" className="search-bar-input"/>
+        <h4 className="search-bar-h4">STATS BY COUNTRY</h4>
+    </div>
   </main>
 );
 
