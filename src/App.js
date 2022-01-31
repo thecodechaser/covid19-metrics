e715957a14ea9785e29e17605b6157ec7698b668
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-31 18:51:21 +0530

    Add banner text

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import HomePage from './components/HomePage';
 import './styles/header.css';
 import './styles/app.css';
+import './styles/homePage.css';
 
 function App() {
   const heading = 'COVID19 statestics';
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,7 +4,10 @@ import Banner from '../asset/Banner.jpeg';
 const HomePage =()=>{
     return (
         <main>
-            <img src={Banner} alt="COVID19" />
+            <div className="banner-div">
+            <img src={Banner} alt="COVID19" className="img-banner"/>
+            <h2 className="banner-h2">COVID19 statestics<br></br>Over the world</h2>
+            </div>
         </main>
     );
 };
