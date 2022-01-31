976e0297fa1da79b817096614404bf3084fc1c1b
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-31 20:49:33 +0530

    Fix linter erros

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,24 +3,21 @@ import PropTypes from 'prop-types';
 import { BsFillMicFill } from '@react-icons/all-files/bs/BsFillMicFill';
 import { IoIosSettings } from '@react-icons/all-files/io/IoIosSettings';
 
-const Header = ({heading}) => {
-//   const { heading } = props;
-  return (
-    <header className="header">
-      <div className="header-top">
-        <h3 className="header-h3">{heading}</h3>
-        <div className="header-icons">
-          <BsFillMicFill className="mic icon" />
-          <IoIosSettings className="settings icon" />
-        </div>
+const Header = ({ heading }) => (
+  <header className="header">
+    <div className="header-top">
+      <h3 className="header-h3">{heading}</h3>
+      <div className="header-icons">
+        <BsFillMicFill className="mic icon" />
+        <IoIosSettings className="settings icon" />
       </div>
-    </header>
-  );
-};
+    </div>
+  </header>
+);
 
 Header.propTypes = {
- 
-    heading: PropTypes.string.isRequired,
+
+  heading: PropTypes.string.isRequired,
 };
 
 export default Header;
