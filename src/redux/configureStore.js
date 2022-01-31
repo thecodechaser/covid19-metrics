02bf5f2a8a391f6da691bad8e18b9d323b9fd1f2
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-31 20:19:41 +0530

    Install redux

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import covid19Data from './covid19Data/covid19Data'
+import covid19Data from './covid19Data/covid19Data';
 
 const reducer = combineReducers({
-  covid19Data
+  covid19Data,
 });
 
 const store = createStore(
--- src/redux/covid19Data/covid19Data.js
+++ src/redux/covid19Data/covid19Data.js
@@ -1,8 +1,8 @@
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-      default:
-        return state;
-    }
-  };
-  
-  export default reducer;
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
