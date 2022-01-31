5325afe3b51812ffd5ba8b75132df6d3c1cbc2c3
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-31 18:25:05 +0530

    Fix linter erros

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Header from "./components/Header";
+import Header from './components/Header';
 import './styles/header.css';
 import './styles/app.css';
 
 function App() {
-  const heading = "COVID19 statestics";
+  const heading = 'COVID19 statestics';
   return (
     <div className="App">
-      <Header heading={heading}/>
+      <Header heading={heading} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,27 @@
-import React from "react";
-import { BsFillMicFill } from "@react-icons/all-files/bs/BsFillMicFill";
-import { IoIosSettings } from "@react-icons/all-files/io/IoIosSettings";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { BsFillMicFill } from '@react-icons/all-files/bs/BsFillMicFill';
+import { IoIosSettings } from '@react-icons/all-files/io/IoIosSettings';
 
-const Header = (props)=>{
-    const heading = props.heading;
-    return (
-        <header className="header">
-            <div className="header-top">
-                <h3 className="header-h3">{heading}</h3>
-                <div className="header-icons">
-                <BsFillMicFill className="mic icon"/>
-                <IoIosSettings className="settings icon"/>
-                </div>
-            </div>
-        </header>
-    );
+const Header = (props) => {
+  const { heading } = props;
+  return (
+    <header className="header">
+      <div className="header-top">
+        <h3 className="header-h3">{heading}</h3>
+        <div className="header-icons">
+          <BsFillMicFill className="mic icon" />
+          <IoIosSettings className="settings icon" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+Header.propTypes = {
+  heading: PropTypes.shape({
+    heading: PropTypes.string,
+  }).isRequired,
 };
 
 export default Header;
