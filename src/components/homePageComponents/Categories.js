047911980b7e81103f60561e2871ac483a85681e
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-02-01 13:03:29 +0530

    Import arrow icon

--- src/components/homePageComponents/Categories.js
+++ src/components/homePageComponents/Categories.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { BsArrowRightCircle } from '@react-icons/all-files/bs/BsArrowRightCircle';
 
 const Categories = (props) => {
   const { data: { country, infected } } = props;
   return (
     <div className="category-top">
-      <h1>{country}</h1>
-      <h1>{infected}</h1>
+      <h1 className="category-country">{country}</h1>
+      <h1 className="category-infected">{infected}</h1>
     </div>
   );
 };
