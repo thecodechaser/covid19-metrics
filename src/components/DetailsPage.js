b13b721e7a311bdaadb9b898cbf04e0592958104
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-02-02 01:20:20 +0530

    Add functionality to back icon

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router';
 import { useSelector } from 'react-redux';
 import { IoMdArrowRoundBack } from '@react-icons/all-files/io/IoMdArrowRoundBack';
 import Header from './Header';
+import { NavLink } from 'react-router-dom';
 
 const DetailsPage = () => {
   const covid19Data = useSelector((state) => state.covid19Data);
@@ -13,7 +14,8 @@ const DetailsPage = () => {
     <>
       <Header heading={countryName} />
       <div>
-          <IoMdArrowRoundBack className="back-icon"/>
+          <NavLink to={"/"}>
+          <IoMdArrowRoundBack className="back-icon"/></NavLink>
         <h1>Hello</h1>
       </div>
     </>
