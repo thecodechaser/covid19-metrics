ab8b2b73511f204978c912920f1c429cc19b6862
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-02-02 01:15:05 +0530

    Add back icon in details page

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { useLocation } from 'react-router';
-import Header from './Header';
 import { useSelector } from 'react-redux';
+import { IoMdArrowRoundBack } from '@react-icons/all-files/io/IoMdArrowRoundBack';
+import Header from './Header';
 
 const DetailsPage = () => {
-    const covid19Data = useSelector((state)=> state.covid19Data);
+  const covid19Data = useSelector((state) => state.covid19Data);
   const data = useLocation();
   const countryName = data.pathname.substring(1).replace(/[^a-zA-Z. ]/g, ' ');
-  const countryData = covid19Data.filter((data) => data.Country.localeCompare(countryName)===0);
+  const countryData = covid19Data.filter((data) => data.Country.localeCompare(countryName) === 0);
   return (
     <>
       <Header heading={countryName} />
       <div>
+          <IoMdArrowRoundBack className="back-icon"/>
         <h1>Hello</h1>
       </div>
     </>
