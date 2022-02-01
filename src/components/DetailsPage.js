cd1b59d38952f923349f2e86ea7650165a83c248
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-02-01 23:24:01 +0530

    Import useSelector in DetailsPage

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router';
 import Header from './Header';
+import { useSelector } from 'react-redux';
 
 const DetailsPage = () => {
   const data = useLocation();
