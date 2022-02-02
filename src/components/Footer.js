78eddf8a6f2256ac4a42c5b26fcbc472f7d231c4
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-02-02 14:44:32 +0530

    Create Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer =()=>{
+    return (
+        <footer>
+            <h2>2022 All Rights Reserved</h2>
+        </footer>
+    );
+};
+
+export default Footer;
