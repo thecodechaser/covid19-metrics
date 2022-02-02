1438d9a08c91163e8ae87d9cb14e235b8f19577f
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-02-02 14:59:25 +0530

    Add styling to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import React from "react";
 const Footer =()=>{
     return (
         <footer className="footer">
-            <h3>
+            <h3 className="footer-h3">
       <span>Created By</span>
-      <a class="footer-link" href="https://github.com/thecodechaser" >Ranjeet Singh</a>
+      <a className="footer-link" href="https://github.com/thecodechaser"> Ranjeet Singh</a>
   </h3>
         </footer>
     );
