c6354a4ee06b3841d7a8735b1602886b48df4ced
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-02-02 15:43:05 +0530

    Add test for Countries component

---
+++ src/__test__/Categories.test.js
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import Categories from '../components/homePageComponents/Categories';
+
+describe('Categories tests', () => {
+  test('should render', () => {
+      const data ={
+          country: 'India',
+          TotalConfirmed: 1000,
+      }
+    const categories = render(
+      <BrowserRouter>
+        <Categories data={data}/>
+      </BrowserRouter>,
+    );
+
+    expect(categories).toMatchSnapshot();
+  });
+});
